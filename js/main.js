ecf5e83573443424f139cd0dcc2368330d58d151
Jon Kostyniuk <jon.kostyniuk@gmail.com>
2015-05-31 15:22:22 -0400

    Development Commit

--- js/main.js
+++ js/main.js
@@ -1,71 +1,116 @@
-// Initialize Variables
-var map = null;		// Google Map Variable
-var listClk = null;	// ID on List Click
+// ##########################################################################################################
+// GTFS CLIENT-SIDE APPLICATION
+// Created by Jon Kostyniuk on 28MAY2015
+// Last modified by Jon Kostyniuk on 28MAY2015
+// Property of JK Enterprises
+// v0.01a
+// ##########################################################################################################
+//
+// Version History:
+// ----------------
+// 28MAY2015 v0.01a - JK
+//   - Initial Version.
+//
+// Usage:
+// ------
+// Describe usage.
+//
+// Instructions:
+// -------------
+// None currently.
+//
+
+// ##########################################################################################################
+// DEFINITIONS
+// ##########################################################################################################
+
+// Map Variables
+var $map = null;		// Google Map Variable
+
+// GTFS Data Variables
+var $SelAgency = null;	// Selected Transit Agency ID
+
+// GUI Variables
+var $diaName = null;		// Name of Dialogue Window Open
 
 
+// ##########################################################################################################
+// DEFINED FUNCTIONS
+// ##########################################################################################################
 
-// On Page Load, Initialize Below
+// MODULE FUNCTIONS
+// ----------------
+
+// Function to Initialize on On Page Load
 function initialize() {
-	
 	// Create New Google Maps Object
-	var mapOptions = {
+	var $mapOptions = {
 	    zoom: 12,
 	    center: new google.maps.LatLng(43.809785, -79.454099)
 		};
-	map = new google.maps.Map($('#gmap')[0], mapOptions);
+	$map = new google.maps.Map($('#gmap')[0], $mapOptions);
 
 	// Event Handler to Load Selected Agency on Click
 	$('#load-agency').on('click', function (e) {
-		$('#load-agency').prop('disabled', true);
-		$('#mAgency').modal('hide');
-	    alert(listClk);
+		loadAgency();
 	})
 
 	// Event Handler to Select Agency on Click
 	$('#select-agency').on('click', function (e) {
-
-		//your awesome code here
-	    //alert("The paragraph was clicked.");
-	    $("#mAgency").modal('show');
+	    $("#mAgency").modal('show'); // Load dialogue window
+	    $diaName = "Agency"; // Set active dialogue name
 	})
-
-
-
 }
 
-
+// Function to Clear Agency
 function clearAgency() {
-	listClk = null;
+	$SelAgency = null; // Clear Selected Agency
+	$(".list-item").css("color", "#000000"); // Clear selected styles
+	$(".list-item").css("background", "#ffffff"); // Clear selected styles
+	$('#load-agency').prop('disabled', true); // Disable submit button
+	$('#mAgency').modal('hide'); // Hide dialogue
+	$diaName = null; // Clear active dialogue name
+}
+
+// Function to Load Agency
+function loadAgency() {
+	$(".list-item").css("color", "#000000"); // Clear selected styles
+	$(".list-item").css("background", "#ffffff"); // Clear selected styles
 	$('#load-agency').prop('disabled', true);
 	$('#mAgency').modal('hide');
-	
+	$diaName = null;
+    alert($SelAgency);
 }
 
 // Function to Handle List Clicks
-function listClick(listID) {
-	listClk = listID
-	$('#load-agency').prop('disabled', false);
+function listClick($listID) {
+	// Determine which dialogue to handle
+	switch($diaName) {
+	    case "Agency": // Agency Select Dialogue Window
+			$SelAgency = $listID
+			$('#load-agency').prop('disabled', false);
+	        break;
+	    // Add more cases as needed
+	}
 
-	// Need to add detail to change selection color
+	// Clear selected styles
+	$(".list-item").css("color", "#000000");
+	$(".list-item").css("background", "#ffffff");
+	// Add selected item style
+	$("#" + $listID).css("color", "#ffffff");
+	$("#" + $listID).css("background", "#0000ff");
 }
 
-/*
-function myPopup() {
-	alert( "xyz" );
-}
 
-for(var i = 0; i < listitem.length; i++) listitem[i].onclick = myPopup;
-*/
+// HELPER (MONKEY) FUNCTIONS
+// -------------------------
+
+// n/a
 
 
+// ##########################################################################################################
+// MAIN PROGRAM
+// ##########################################################################################################
+
+// Initialize Google Maps DOM Listener
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-/*
-var $lba= document.getElementsByClassName('login-button-arrow');
-
-function myPopup(){
-  alert( "xyz" );
-}
-
-for(var i=0; i<$lba.length; i++) $lba[i].onclick = myPopup;*/
